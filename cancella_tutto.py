03b04ae9ac3e010640640807bb2c4d55319f6d14
mmamei <marco.mamei@unimore.it>
2024-10-07 15:28:12 +0200

    esercizi

---
+++ cancella_tutto.py
@@ -0,0 +1,6 @@
+a = [1,2,3,3,4,5,3,2,1]
+c = 3
+# voglio cancellare tutti i 3, usando il comando remove
+while c in a:
+    a.remove(c)
+print(a)
--- test.py
+++ test.py
@@ -1,9 +1,1 @@
 a = [1,2,3,4,5]
-b = []
-for x in a:
-    b.append(x)
-
-
-
-b[0] = 2
-print(a)
