e6b5dfed2a6eb81f835472cacf317a88aa2075df
mmamei <marco.mamei@unimore.it>
2024-10-14 16:25:49 +0200

    esercizio

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-sv = {
-    'Marco Mamei':27,
-    'Roberta Rossetto':24
+
+a = {
+    'marco':[1,200,3,4],
+    'anna':[6],
+    'luca':[6]
 }
 
-sv['Marco Mamei'] = 30
-print(sv)
+b = sorted(a.items(), key=lambda x:sum(x[1])/len(x[1]), reverse=True)
+print(b)
+
+
+
