3c3502665ac18c714ab7f7f8099286dd7858f1e4
mmamei <marco.mamei@unimore.it>
2024-10-17 10:14:46 +0200

    ese

--- esame1/esame.py
+++ esame1/esame.py
@@ -1,3 +1,19 @@
+'''
+Data una lista di messaggi rappresentati da un una lista di dizionari del tipo:
+{'data':'14-10-2024','da':'marco','a':'franco','testo':'ciao!'}
+Creare un programma python, senza usare le funzioni, che esegua i seguenti punti:
+1. Trovare la persona che scrive più messaggi
+2. Per ogni persona, stamapre il numero di persone con cui comunica
+3. Scrivere la tabella che riassume il numero di messaggi inviati da una persona all'altra
+
+Da\A    Marco   Matteo  Luca
+Marco   0       1       2
+Matteo  2       0       1       
+Luca    3       1       0
+'''
+
+
+
 messaggi = [
     {'data':'14-10-2024','da':'marco','a':'franco','testo':'ciao!'},
     {'data':'14-10-2024','da':'franco','a':'marco','testo':'ciao! tutto bene?'},
