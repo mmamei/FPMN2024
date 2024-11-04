6b89c18cdadfaf58e009a80860e63863d0f357a5
mmamei <marco.mamei@unimore.it>
2024-11-04 15:28:19 +0100

    add

--- menu-oo.py
+++ menu-oo.py
@@ -14,13 +14,18 @@ class Aula():
         self.studenti = []
     def aggiungi(self,s):
         self.studenti.append(s)
+    def cerca(self,nome):
+        for s in self.studenti:
+            if s.nome == nome:
+                return s
+        return None
 
 class Menu():
     def __init__(self):
         self.aula = Aula()
     def stampa(self):
         print("1. Inserisci Studente")
-        print("2. Inserisci media voti studente")
+        print("2. Inserisci voto studente")
         print("3. Stampa tutto")
         print("4. Calcola media voti della classe")
         print("5. Esci")
@@ -33,11 +38,17 @@ class Menu():
                 s = Studente(nome)
                 self.aula.aggiungi(s)
             if op == '2':
-                ...
+                nome = input('Inserisci nome: ')
+                s = self.aula.cerca(nome)
+                v = int(input('Inserisci voto: '))
+                s.aggiungi_voto(v)
             if op == '3':
-                ...
+                for s in self.aula.studenti:
+                    print(s.stampa())
             if op == '4':
-                ...
+                nome = input('Inserisci nome: ')
+                s = self.aula.cerca(nome)
+                print(s.calcola_media())
             if op == '5':
                 break
 
