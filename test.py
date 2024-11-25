bc82c1a69735eee32feb6321434f5746694f4b0d
mmamei <marco.mamei@unimore.it>
2024-11-25 10:45:33 +0100

    game

--- test.py
+++ test.py
@@ -1,4 +1,80 @@
-lst = [1, 2, 3, 4, 5]
-result = [lst[i] - lst[-(i+1)] for i 
-in range(len(lst)//2)]
-print(result)
+
+import pygame
+from pygame.locals import K_UP,K_DOWN,K_LEFT,K_RIGHT,K_ESCAPE,KEYDOWN,QUIT
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+        self.rect.left = SCREEN_WIDTH / 2
+        self.rect.top = SCREEN_HEIGHT / 2
+
+    # Move the sprite based on user keypresses
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+        
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+# Initialize pygame
+pygame.init()
+clock = pygame.time.Clock()
+
+player = Player()
+
+
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
+
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    # Fill the screen with black
+    screen.fill((0, 0, 0))
+
+    screen.blit(player.surf, player.rect)
+
+    pygame.display.flip()
+    # Ensure program maintains a rate of 30 frames per second
+    clock.tick(30)
+
+
