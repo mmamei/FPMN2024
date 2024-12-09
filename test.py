28362dc2d6155da962d871f66493717c8d134c4c
mmamei <marco.mamei@unimore.it>
2024-12-09 17:39:54 +0100

    photo recall bot

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-from datetime import datetime
-s = datetime.strptime('09-19-2022 08:00:00', '%m-%d-%Y %H:%M:%S')
-e = datetime.strptime('09-19-2022 18:00:00', '%m-%d-%Y %H:%M:%S')
-d = datetime.strptime('09-19-2022 10:00:00', '%m-%d-%Y %H:%M:%S')
+from datetime import datetime, timedelta
+s = datetime.strptime('19092022 08:00:00', '%d%m%Y %H:%M:%S')
+e = datetime.strptime('19092022 18:00:00', '%d%m%Y %H:%M:%S')
+d = datetime.strptime('19092022 10:00:00', '%d%m%Y %H:%M:%S')
+
 if s < d < e:
     print('ciao')
+
+s = datetime.strftime(s,'%d %B %Y')
+print(s)
+
+print(d.weekday())
+
+print(d + timedelta(days=1))
